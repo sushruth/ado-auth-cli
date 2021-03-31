05419f9b8c0d3d8991ef17ab710e30db7ad98f4b
Sushruth Sastry <susastry@microsoft.com>
2021-03-30 18:39:26 -0700

    Fixing the server

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -16,11 +16,21 @@ export async function listenForTokenFromTheWebsite() {
     let server: http.Server | undefined;
 
     app.use(async (ctx) => {
-      const body = ctx.request.body;
-      console.log("Got body", body);
-      ctx.body = { status: "OK" };
-      resolve(body);
-      server?.close();
+      if (/options/i.test(ctx.method)) {
+        ctx.set("Access-Control-Allow-Origin", "https://ado-auth.vercel.app");
+        ctx.set("Access-Control-Allow-Methods", "POST, OPTIONS");
+        ctx.set("Access-Control-Allow-Headers", "content-type");
+        ctx.status = 200;
+        ctx.res.end();
+      } else {
+        const body = ctx.request.body;
+        console.log("Got body", body);
+        ctx.body = { status: "OK" };
+        ctx.status = 200;
+        ctx.res.end();
+        setImmediate(() => server?.close());
+        resolve(body);
+      }
     });
 
     server = app.listen(35287);
